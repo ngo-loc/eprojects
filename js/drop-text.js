afc46ca3cda6c78210d091d2d301a667e3b0781f
Admin <you@example.com>
2017-12-14 13:00:06 +0700

    project

---
+++ js/drop-text.js
@@ -0,0 +1,21 @@
+function demofunction(){
+	document.getElementById('demo').style.display='block';
+	document.getElementById('demo1').style.display='none';
+	document.getElementById('demo2').style.display='none';
+	document.getElementById('demo3').style.display='none';
+};
+function demo1function() {
+	document.getElementById('demo1').style.display='block';
+};
+// function demo2function(){
+// 	document.getElementById('demo').style.display='none';
+// 	document.getElementById('demo1').style.display='none';
+// 	document.getElementById('demo2').style.display='block';
+// 	document.getElementById('demo3').style.display='none';
+// };
+// function demo3function(){
+// 	document.getElementById('demo').style.display='none';
+// 	document.getElementById('demo1').style.display='none';
+// 	document.getElementById('demo2').style.display='none';
+// 	document.getElementById('demo3').style.display='block';
+// };
